153f602d7d8670cdcbc7321acdd62180aee8da1d
Jhowrf <jhowrf@outlook.com>
2023-03-02 13:52:16 -0300

    Adicionado método para buscar etiqueta de pedido no anymarket.

--- src/Anymarket/Anymarket.php
+++ src/Anymarket/Anymarket.php
@@ -107,6 +107,25 @@ class Anymarket
         return $this->send('GET', "orders/$orderId");
     }
 
+    public function getOrderTag(int $orderId)
+    {
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+            CURLOPT_URL => "http://api.anymarket.com.br/v2/printtag/PDF",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 20,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => "{\"orders\":[" . $orderId . "]}",
+            CURLOPT_HTTPHEADER => ["Content-Type: application/json", "gumgaToken: " . $this->token]
+        ]);
+        $response = curl_exec($curl);
+        curl_close($curl);
+        return $response;
+    }
+
     /**
      * @param array $parameters
      * @return StandardResponse
